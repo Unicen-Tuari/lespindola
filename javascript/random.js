ab117769907eef2a08e6ef96c52e00090f8fe7a7
Lucas Nahuel Espindola <lucas.nahuel.espindola1998@gmail.com>
2020-04-30 21:16:27 -0300

    Meto el random en funciones

--- javascript/random.js
+++ javascript/random.js
@@ -4,16 +4,21 @@ const productos = ["Procesadores", "Almacenamiento", "Mothers",
  "Placas de Video", "Memorias RAM", "Fuentes de Poder", "Gabinetes"];
 
 
-const aleatorio = (max) => Math.floor(Math.random() * max);
-
-const obteneraleatorio = () => productos[aleatorio(productos.length)];
-
-const mostraraleatorio = () => {
-  document.getElementById('random').innerHTML = obteneraleatorio();
+function aleatorio(){
+    Math.floor(Math.random() * max);
 }
 
-document.getElementById('generar')
-  .addEventListener('click', mostraraleatorio);
+function obteneraleatorio(){
+  productos[aleatorio(productos.length)];
+}
 
-mostraraleatorio();
+function mostraraleatorio(){
+   document.getElementById('random').innerHTML = obteneraleatorio();
+  }
+
+
+document.getElementById('generar')
+  .addEventListener('click', mostraraleatorio());
+
+mostraraleatorio(productos);
 
