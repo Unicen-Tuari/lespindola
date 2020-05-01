fc7542d1e31da230c45af0b7f9ac8b219a2c6093
Lucas Nahuel Espindola <lucas.nahuel.espindola1998@gmail.com>
2020-04-30 22:26:03 -0300

    Agrego random

--- javascript/random.js
+++ javascript/random.js
@@ -4,21 +4,16 @@ const productos = ["Procesadores", "Almacenamiento", "Mothers",
  "Placas de Video", "Memorias RAM", "Fuentes de Poder", "Gabinetes"];
 
 
-function aleatorio(){
-    Math.floor(Math.random() * max);
+const aleatorio = (max) => Math.floor(Math.random() * max);
+
+const obteneraleatorio = () => productos[aleatorio(productos.length)];
+
+const mostraraleatorio = () => {
+  document.getElementById('random').innerHTML = obteneraleatorio();
 }
-
-function obteneraleatorio(){
-  productos[aleatorio(productos.length)];
-}
-
-function mostraraleatorio(){
-   document.getElementById('random').innerHTML = obteneraleatorio();
-  }
-
 
 document.getElementById('generar')
-  .addEventListener('click', mostraraleatorio());
+  .addEventListener('click', mostraraleatorio);
 
-mostraraleatorio(productos);
+mostraraleatorio();
 
