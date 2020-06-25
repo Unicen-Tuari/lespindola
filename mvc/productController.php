09745607c2ad29bcbcb41724d26176e76d29ce83
Lucas Nahuel Espindola <lucas.nahuel.espindola1998@gmail.com>
2020-06-25 02:10:54 -0300

    Hice mvc, separe el codigo, en model, view, controller, me estaria faltando la parte de session

--- mvc/productController.php
+++ mvc/productController.php
@@ -19,6 +19,56 @@ class productController{
         $this->view->products($products, $categories);
       }
 
+      function viewCategories($member = null){
+        $products = $this->model->getProduct();
+        $categories = $this->model->getCategories();
+        $this->view->viewCategories($products, $categories);
+      }
+
+      function VentaDeHardware($member = null){
+        $products = $this->model->getProduct();
+        $categories = $this->model->getCategories();
+        $this->view->VentaDeHardware($products, $categories);
+      }
+      
+      function viewProducts($params){
+        $products = $this->model->getProductByCategory($params[0]);
+        $category =$this->model-> getCategory($params[0]);
+        $categories = $this->model->getCategories();
+        $this->view->viewProducts($products, $categories, $category);
+      }
+      
+      function viewSale($member = null){
+        $products = $this->model->getProduct();
+        $this->view->viewSale($products);
+      }
+
+      function insertProducts(){
+        $this->model->createProduct($_GET["title"], $_GET["category"], $_GET["description"]); 
+        header ("location: home");
+      }
+      
+      function insertCategory(){
+        $this->model->createCategory($_GET["id_category"], $_GET["name"]); 
+        header ("location: categories");
+      }
+      
+      function deleteProduct($params){
+        $this->model->removeProduct($params[0]); 
+        header ("location: ../home");
+      }
+      
+      function deleteCategory($params){
+        $this->model->removeCategory($params[0]);
+        header ("location: ../categories");
+      }
+      
+      function complete($params){
+        $this->model->markProductAsDone($params[0]);
+        header ("location: ../home");
+      }
+      
+
 }
 
 ?>
