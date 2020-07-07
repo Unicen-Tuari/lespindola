0b22946d6b3ad8f38ecbd2a788c54d6a1e8f37d1
Lucas Nahuel Espindola <lucas.nahuel.espindola1998@gmail.com>
2020-07-07 15:28:27 -0300

    Base de datos

--- mvc/hardwareSales.sql
+++ mvc/hardwareSales.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jun 14, 2020 at 11:57 PM
+-- Generation Time: Jul 07, 2020 at 08:24 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.6
 
@@ -24,48 +24,115 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `id_category` int(11) NOT NULL,
+  `name` varchar(200) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`id_category`, `name`) VALUES
+(27, 'Procesadores'),
+(28, 'Almacenamiento'),
+(29, 'Mothers'),
+(30, 'Placas de Video'),
+(31, 'Memorias RAM'),
+(32, 'Fuentes de Poder'),
+(33, 'Gabinetes');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `product`
 --
 
 CREATE TABLE `product` (
   `id_product` int(11) NOT NULL,
   `title` varchar(50) COLLATE utf8_bin NOT NULL,
-  `category` varchar(60) COLLATE utf8_bin NOT NULL,
   `description` varchar(200) COLLATE utf8_bin DEFAULT NULL,
-  `done` tinyint(4) NOT NULL DEFAULT 0
+  `done` tinyint(4) NOT NULL DEFAULT 0,
+  `fk_category` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dumping data for table `product`
 --
 
-INSERT INTO `product` (`id_product`, `title`, `category`, `description`, `done`) VALUES
-(24, 'Product 123', 'dasdad', 'asdasd', 1),
-(25, 'sfafa', 'asdfghj', 'rtwez465yrcv8967trc', 1),
-(26, 'Mother gigabyte', 'Mother', 'la mejor de todas', 0),
-(27, 'Processors APU A4', 'Processors', 'el mejor de la life', 0),
-(28, 'Gamemax 700W', 'Sources', 'potente como caballo', 0),
-(29, 'SSD Patriot', 'Discs', 'Vuela como avioneta', 0);
+INSERT INTO `product` (`id_product`, `title`, `description`, `done`, `fk_category`) VALUES
+(56, 'Product 1234567', 'sadasd', 1, 27);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `email` varchar(50) COLLATE utf8_bin NOT NULL,
+  `password` varchar(100) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `email`, `password`) VALUES
+(7, 'lucas@xn--ucas-z1a', '$2y$10$ZqHPLDBzj9/Yp4px4LRGjeq4vl.hUj9sm9Wwtbj9hfsaiyHUnGbMu'),
+(8, 'lucas5@lucas', '$2y$10$x/HMcZIVNBEDE4HeylT1UuYKMeKZTJQ9tmhQmiYXMG7XRE7GNTW/a'),
+(9, 'lucas4@lucas', '$2y$10$MLDpXSC60u.XwLPqX6YJL.FH/sYKmdlXH/M3GMSAS8uziIdAUNCG6'),
+(10, 'lucas4@lucas', '$2y$10$G3Xpr1GqDAmAAhxBVvO7LuMBHwTW5Ezgf6OLnvJt5Cv5.b0oEQ/0C'),
+(11, 'lucas5@lucas', '$2y$10$2k5BSR8YRJtfV70YPOaT9OhWh0wmQm/.Isyv.cbRvwrWXAexg0LrK');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id_category`);
+
+--
 -- Indexes for table `product`
 --
 ALTER TABLE `product`
-  ADD PRIMARY KEY (`id_product`);
+  ADD PRIMARY KEY (`id_product`),
+  ADD KEY `fk_category` (`fk_category`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id_category` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+
+--
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
